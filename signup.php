22ecc44be98c360bc95985df404dc5e84e2130d4
Philip Sykes <85830485+PhilipSykes@users.noreply.github.com>
2023-03-29 00:22:35 +0100

    forms - front end

--- signup.php
+++ signup.php
@@ -10,7 +10,35 @@
 </head>
 <body>
     <div class="centered-col form">
-        <h1 class="form-header">Sign Up</h1>
+        <h1 class="form-header">TasQ</h1>
+        <form class="form-container" method="post">
+            <div class="form-section">
+                <label class="form-label">Name</label>
+                <div class="name-container">
+                    <input type="text" class="form-text left">
+                    <input type="text" class="form-text">
+                </div>
+            </div>
+            <div class="form-section">
+                <label class="form-label">Username</label>
+                <input type="text" class="form-text">
+            </div>
+            <div class="form-section">
+                <label class="form-label">Email</label>
+                <input type="text" class="form-text">
+            </div>
+            <div class="form-section">
+                <label class="form-label">Password</label>
+                <input type="text" class="form-text">
+            </div>
+            <div class="form-section">
+                <label class="form-label">Confirm Password</label>
+                <input type="text" class="form-text">
+            </div>
+            <div class="form-section submit">
+                <input type="submit" class="button-primary form-button" value="Sign Up!" name="submit">
+            </div>
+        </form>
     </div>
 </body>
 </html>
